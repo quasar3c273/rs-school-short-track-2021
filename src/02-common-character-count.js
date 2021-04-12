155752cea024e145a8fe4cf99edf6b4b3ccae0fe
quasar3c273 <hiimisha@mail.ru>
2021-04-12 22:13:09 +0300

    fix: 2 task

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -1,3 +1,4 @@
+
 /**
  * Given two strings, find the number of common characters between them.
  *
@@ -10,11 +11,16 @@
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  const arr1 = s1.split('');
-  const arr2 = s2.split('');
-  const matched = (arr1.filter((el) => arr2.indexOf(el) > -1)).length;
-
-  return matched;
+  const res = [];
+  const arrS1 = s1.split('');
+  const arrS2 = s2.split('');
+  for (let i = 0; i < arrS1.length; i++) {
+    if (arrS2.includes(arrS1[i])) {
+      arrS2.splice(arrS2.indexOf(arrS1[i]), 1);
+      res.push(arrS1[i]);
+    }
+  }
+  return res.length;
 }
 
 module.exports = getCommonCharacterCount;
